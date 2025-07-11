1f0a9b7414c5280ead02e0e591e0e84fd5ffce3c
Pavlo Lekhitskyi <pashokman1@gmail.com>
2025-07-11 17:21:17 +0300

    added create_cars, get_cars, create_driver_licence methods, fixed some minor mistakes in previous methods

--- testing/locust/user_behavior.py
+++ testing/locust/user_behavior.py
@@ -4,13 +4,15 @@ import os
 from locust import SequentialTaskSet, task
 from locust.exception import StopUser
 
+from testing.utils.generators.car_generator import generate_car
+from testing.utils.generators.driver_licence_generator import generate_driver_licence
 from testing.utils.generators.garage_generator import generate_garage
 from testing.utils.generators.user_generator import generate_user
 from testing.utils.generators.house_generator import generate_house
 from testing.utils.logs.locust_main_logger import Logger
 from testing.utils.logs.locust_response_err_log import response_err_log
 
-# clear log files before each run
+# clear log files before each locust run from the command line
 for log_file in ["locust_main.log", "locust_only_error.log"]:
     if os.path.exists(log_file):
         with open(log_file, "w"):
@@ -30,6 +32,7 @@ class UserBehavior(SequentialTaskSet):
         self.user_token = None
         self.house_ids = []
         self.garage_ids = []
+        self.car_ids = []
 
     def on_stop(self):
         self.user_obj = None
@@ -37,6 +40,7 @@ class UserBehavior(SequentialTaskSet):
         self.user_token = None
         self.house_ids = []
         self.garage_ids = []
+        self.car_ids = []
 
     def create_user(self):
         self.user_obj = generate_user(password_length=8)
@@ -71,7 +75,7 @@ class UserBehavior(SequentialTaskSet):
             with self.client.post(
                 "/houses",
                 data=json.dumps(generate_house()),
-                name="002. Create house",
+                name="003. Create house",
                 headers=self.headers,
                 catch_response=True,
             ) as response:
@@ -85,7 +89,7 @@ class UserBehavior(SequentialTaskSet):
     def get_houses(self):
         with self.client.get(
             "/houses",
-            name="003. Get houses",
+            name="004. Get houses",
             headers=self.headers,
             catch_response=True,
         ) as response:
@@ -97,6 +101,17 @@ class UserBehavior(SequentialTaskSet):
     def create_garages(
         self, total_garage_count: int = 4, house1_related_garage_count: int = 1, house2_related_garage_count: int = 1
     ):
+        """
+        Create garages.
+
+        Args:
+            total_garage_count (int): The total number of garages to create. Default = 4.
+            house1_related_garage_count (int): How many garages should be created with house1 relation. Default = 1.
+            house2_related_garage_count (int): How many garages should be created with house2 relation. Default = 1.
+
+        Returns:
+            None
+        """
         if house1_related_garage_count + house2_related_garage_count > total_garage_count:
             raise ValueError(
                 f"Invalid configuration: total_garage_count ({total_garage_count}) < house1_related_garage_count ({house1_related_garage_count}) + house2_related_garage_count ({house2_related_garage_count})"
@@ -110,7 +125,7 @@ class UserBehavior(SequentialTaskSet):
             with self.client.post(
                 "/garages",
                 data=json.dumps(garage),
-                name=f"004. Create garage {index} related to {relation}",
+                name=f"005. Create garage {index} related to {relation}",
                 headers=self.headers,
                 catch_response=True,
             ) as response:
@@ -126,7 +141,7 @@ class UserBehavior(SequentialTaskSet):
         for i in range(house1_related_garage_count):
             create_garage_request(i, self.house_ids[0], "house1")
 
-        for j in range(house1_related_garage_count):
+        for j in range(house2_related_garage_count):
             create_garage_request(j, self.house_ids[1], "house2")
 
         rest = total_garage_count - house1_related_garage_count - house2_related_garage_count
@@ -136,12 +151,89 @@ class UserBehavior(SequentialTaskSet):
     def get_garages(self):
         with self.client.get(
             "/garages",
-            name="005. Get garages",
+            name="006. Get garages",
             headers=self.headers,
             catch_response=True,
         ) as response:
             if response.status_code == 200:
                 UserLog.info(f'Got garages - {response.text}, token - "{self.user_token}"')
+            else:
+                response_err_log(response)
+
+    def create_cars(
+        self, total_car_count: int = 4, garage1_related_car_count: int = 1, garage2_related_car_count: int = 1
+    ):
+        """
+        Create cars.
+
+        Args:
+            total_car_count (int): The total number of cars to create. Default = 4.
+            garage1_related_car_count (int): How many cars should be created with garage1 relation. Default = 1.
+            garage2_related_car_count (int): How many cars should be created with garage2 relation. Default = 1.
+
+        Returns:
+            None
+        """
+        if garage1_related_car_count + garage2_related_car_count > total_car_count:
+            raise ValueError(
+                f"Invalid configuration: total_car_count ({total_car_count}) < garage1_related_car_count ({garage1_related_car_count}) + garage2_related_car_count ({garage2_related_car_count})"
+            )
+
+        def create_car_request(index: int, garage_id: str | None, relation: str):
+            car = generate_car()
+            if garage_id:
+                car["garage_id"] = garage_id
+
+            with self.client.post(
+                "/cars",
+                data=json.dumps(car),
+                name=f"007. Create car {index} related to {relation}",
+                headers=self.headers,
+                catch_response=True,
+            ) as response:
+                if response.status_code == 200:
+                    car_id = response.json()["id"]
+                    self.car_ids.append(car_id)
+                    UserLog.info(
+                        f'Car {index} created, car_id = {car_id}, garage_id - "{car["garage_id"]}", token - "{self.user_token}"'
+                    )
+                else:
+                    response_err_log(response)
+
+        for i in range(garage1_related_car_count):
+            create_car_request(i, self.garage_ids[0], "garage1")
+
+        for j in range(garage2_related_car_count):
+            create_car_request(j, self.garage_ids[1], "garage2")
+
+        rest = total_car_count - garage1_related_car_count - garage2_related_car_count
+        for k in range(rest):
+            create_car_request(k, None, "no garage")
+
+    def get_cars(self):
+        with self.client.get(
+            "/cars",
+            name="008. Get cars",
+            headers=self.headers,
+            catch_response=True,
+        ) as response:
+            if response.status_code == 200:
+                UserLog.info(f'Got cars - {response.text}, token - "{self.user_token}"')
+            else:
+                response_err_log(response)
+
+    def create_driver_licence(self):
+        with self.client.post(
+            "/licences",
+            data=json.dumps(generate_driver_licence()),
+            name="009. Create driver licence",
+            headers=self.headers,
+            catch_response=True,
+        ) as response:
+            if response.status_code == 200:
+                UserLog.info(
+                    f'Driver licence created, licence id - {response.json()["id"]}, token - "{self.user_token}"'
+                )
             else:
                 response_err_log(response)
 
@@ -176,5 +268,8 @@ class UserBehavior(SequentialTaskSet):
         self.get_houses()
         self.create_garages()  # add getting variables from console
         self.get_garages()
+        self.create_cars()  # add getting variables from console
+        self.get_cars()
+        self.create_driver_licence()
 
         self.interrupt()
